fc386b6a13bd812795282665de9a0f3c906146b9
digitalthink-pruebas <digitalthink.es-pruebas@gmail.com>
2021-01-08 13:34:58 +0100

    Error Test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -17,5 +17,9 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+
+        $response = $this->get('/error');
+
+        $response->assertStatus(200);
     }
 }
